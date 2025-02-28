bbd598014d98fbdbff2be28ad5a142ac66609110
Marcap00 <cappellatomarco@yahoo.com>
2025-02-28 15:38:45 +0100

    Format SignIn component

--- app/SIgnIn.tsx
+++ app/SIgnIn.tsx
@@ -2,11 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const SignIn = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>SignIn</Text>
+        </View>
+    )
 }
 
 export default SignIn
