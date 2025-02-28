e6ef841bfc3a541affb7f787493863d57cb26029
Marcap00 <cappellatomarco@yahoo.com>
2025-02-28 16:06:06 +0100

    Add navigation links to the main index screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,19 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Link href={"/signIn"}>Sign In</Link>
+            <Link href={"/explore"}>Explore</Link>
+            <Link href={"/profile"}>Profile</Link>
+            <Link href={"/properties/1"}>Property</Link>
+        </View>
+    );
 }
