a31369b13ff5e33804b760928e5103210682ad78
Marcap00 <cappellatomarco@yahoo.com>
2025-03-01 19:53:22 +0100

    Enhance Search component with input field and filter icon for improved user experience

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useLocalSearchParams, usePathname } from 'expo-router'
+import icons from '@/constants/icons';
 
 const Search = () => {
+    const path = usePathname();
+    const params = useLocalSearchParams<{ query: string }>();
+    const [search, setSearch] = useState(params.query);
+
+    const handleSearch = (text: string) => {
+        setSearch(text);
+    }
+
     return (
-        <View>
-            <Text>Search</Text>
-        </View>
+        <View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
+            <View className='flex-1 flex flex-row items-center justify-start z-50'>
+                <Image source={icons.search} className='size-5' />
+                <TextInput
+                    value={search}
+                    onChangeText={handleSearch}
+                    placeholder='Search for your dream home'
+                    placeholderTextColor={'#C8C8C8'}
+                    className='text-sm font-rubik text-black-300 ml-2 flex placeholder:thumb-violet-950'
+                />
+            </View>
+            <TouchableOpacity>
+                <Image source={icons.filter} className='size-5' />
+            </TouchableOpacity>
+        </View >
     )
 }
 
