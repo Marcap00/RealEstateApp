2ed14ddc845a13dc3eb4995808c00bdae7d3d7d7
Marcap00 <cappellatomarco@yahoo.com>
2025-03-09 16:14:29 +0100

    feat(tabs/index): enhance layout by adding FeaturedCard, RegularCard, and Filters components

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,35 +4,58 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
+import { FeaturedCard, RegularCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
       <View className="px-5">
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row items-center">
-            <Image source={images.avatar} className="size-12 rounded-full" />
-            <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">
-                Welcome back
-              </Text>
-              <Text className="text-base font-rubik-medium text-black-300">
-                John Doe
-              </Text>
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full" />
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">
+                  Welcome back
+                </Text>
+                <Text className="text-base font-rubik-medium text-black-300">
+                  John Doe
+                </Text>
+              </View>
             </View>
+            <Image source={icons.bell} className="size-6" />
           </View>
-          <Image source={icons.bell} className="size-6" />
+          <Search />
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See all
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
         </View>
-        <Search />
         <View className="flex flex-row items-center justify-between">
           <Text className="text-xl font-rubik-bold text-black-300">
-            Featured
+            Our recommendations
           </Text>
           <TouchableOpacity>
             <Text className="text-base font-rubik-bold text-primary-300">
               See all
             </Text>
           </TouchableOpacity>
+        </View>
+        <Filters />
+        <View className="flex flex-row flex-wrap gap-5 mt-5">
+          <RegularCard />
+          <RegularCard />
         </View>
       </View>
     </SafeAreaView>
