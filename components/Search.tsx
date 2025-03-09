b79d01b3cd2830e252240eb2b090bd1b4e64c821
Marcap00 <cappellatomarco@yahoo.com>
2025-03-09 14:48:20 +0100

    feat(search): implement debouncing in Search component for improved performance

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,34 +1,43 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { useLocalSearchParams, usePathname } from 'expo-router'
-import icons from '@/constants/icons';
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { router, useLocalSearchParams, usePathname } from "expo-router";
+import icons from "@/constants/icons";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = () => {
-    const path = usePathname();
-    const params = useLocalSearchParams<{ query: string }>();
-    const [search, setSearch] = useState(params.query);
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query: string }>();
+  const [search, setSearch] = useState(params.query);
 
-    const handleSearch = (text: string) => {
-        setSearch(text);
-    }
+  const debouncedSearch = useDebouncedCallback(
+    (text: string) => router.setParams({ query: text }),
+    500
+  );
 
-    return (
-        <View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
-            <View className='flex-1 flex flex-row items-center justify-start z-50'>
-                <Image source={icons.search} className='size-5' />
-                <TextInput
-                    value={search}
-                    onChangeText={handleSearch}
-                    placeholder='Search for your dream home'
-                    placeholderTextColor={'#C8C8C8'}
-                    className='text-sm font-rubik text-black-300 ml-2 flex placeholder:thumb-violet-950'
-                />
-            </View>
-            <TouchableOpacity>
-                <Image source={icons.filter} className='size-5' />
-            </TouchableOpacity>
-        </View >
-    )
-}
+  // So if a user types "home" very quickly, the search will only be triggered once after the user stops typing for 500ms
 
-export default Search
+  const handleSearch = (text: string) => {
+    setSearch(text);
+    debouncedSearch(text);
+  };
+
+  return (
+    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
+      <View className="flex-1 flex flex-row items-center justify-start z-50">
+        <Image source={icons.search} className="size-5" />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder="Search for your dream home"
+          placeholderTextColor={"#C8C8C8"}
+          className="text-sm font-rubik text-black-300 ml-2 flex placeholder:thumb-violet-950"
+        />
+      </View>
+      <TouchableOpacity>
+        <Image source={icons.filter} className="size-5" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Search;
